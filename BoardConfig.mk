3ebc3c52ccec06aa9b291b04711fbac202ef7bc2
lutfailham96 <lutfailham96@gmail.com>
2017-04-04 00:55:30 +0700

    Add files via upload

---
+++ BoardConfig.mk
@@ -0,0 +1,68 @@
+#
+# Copyright (C) 2016 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+ 
+LOCAL_PATH := device/lenovo/P70
+
+# ARCHITECTURE
+TARGET_ARCH := arm64
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := armeabi-v7a
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := generic
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+
+TARGET_CPU_SMP := true
+
+# COMPILER FLAGS
+#TARGET_GLOBAL_CFLAGS +=
+#TARGET_GLOBAL_CPPFLAGS +=
+
+# BOOTLOADER
+TARGET_BOARD_PLATFORM := mt6752
+TARGET_BOOTLOADER_BOARD_NAME := mt6752
+TARGET_NO_BOOTLOADER := true
+
+# KERNEL
+BOARD_KERNEL_CMDLINE   := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive androidboot.bootdevice=mtk-msdc.0
+BOARD_KERNEL_BASE      := 0x40080000
+BOARD_KERNEL_PAGESIZE  := 2048
+BOARD_MKBOOTIMG_ARGS   := --kernel_offset 0x00000000 --ramdisk_offset 0x03f80000 --tags_offset 0x0df80000
+TARGET_PREBUILT_KERNEL := device/lenovo/P70/Image.gz
+
+# TARGET IMAGES
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# PARTTIONS
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_FLASH_BLOCK_SIZE := 0x20000
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_REAL_SDCARD := false
+BOARD_SUPPRESS_EMMC_WIPE := true
+
+# TWRP FLAGS
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TW_NO_USB_STORAGE := true
+TW_EXCLUDE_SUPERSU := true
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo omni_P70-eng
+add_lunch_combo omni_P70-userdebug
